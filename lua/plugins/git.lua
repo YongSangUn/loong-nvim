c7a3520b48f284c2339f500aa1c4c5c38b6d7267
Rylan.Loong <sangun.yong@gmail.com>
2025-07-16 16:57:21 +0800

    feat: update git layer.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,25 +1,34 @@
 -- Manages configurations for Neovim plugins that integrate with Git.
 -- Covers Git status indicators, diffs, blame, and other version control operations.
 
-local loong = require('core.loong')
+local loong = require("core.loong")
 
 -- git
 -- https://github.com/NeogitOrg/neogit
-loong.add_plugin('NeogitOrg/neogit', {
-  dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional - Diff integration
-
-    -- Only one of these is needed.
-    'nvim-telescope/telescope.nvim', -- optional
-    -- "ibhagwan/fzf-lua",              -- optional
-    -- "echasnovski/mini.pick",         -- optional
-    -- "folke/snacks.nvim",             -- optional
+loong.add_plugin("NeogitOrg/neogit", {
+  dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+  keys = {
+    { "<leader>gs", ":Neogit<cr>", mode = "n", desc = "Toggle blame window" },
   },
+})
 
-  config = function()
-    require('which-key').add({
-      { '<leader>gs', ':lua require("neogit").open()<cr>', desc = 'Neogit status', mode = 'n' },
-    })
-  end,
+-- blame
+-- https://github.com/lewis6991/gitsigns.nvim
+loong.add_plugin("lewis6991/gitsigns.nvim", {
+  event = { "BufRead", "BufNewFile" },
+  lazy = true,
+  opts = {
+    current_line_blame = true,
+    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+    current_line_blame_opts = { delay = 100 },
+  },
+})
+
+-- https://github.com/FabijanZulj/blame.nvim
+loong.add_plugin("FabijanZulj/blame.nvim", {
+  lazy = false,
+  opts = {}, -- must use opts to setup() or config instead.
+  keys = {
+    { "<leader>gb", ":BlameToggle window<cr>", mode = "n", desc = "Toggle blame window" },
+  },
 })
